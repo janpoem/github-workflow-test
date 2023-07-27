21b9061f2d4771507e05cbb9da2552e150e81e1d
Jiankai Zeng <janpoem@163.com>
2023-07-27 10:29:52 +0800

    1.0.1 再次 publish x 10

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import { helloWorld } from './hello_world';
 
 export default helloWorld;
+
+interface StdInterface {
+
+}
+
+function handleStd<T extends StdInterface>(obj: T) {
+
+}
+
+class TestStd implements StdInterface {
+
+}
+
+handleStd(new TestStd());
